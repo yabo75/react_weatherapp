9c741a5c8686b2c6cc94235759c009404ca57332
Andrea Cardy <acardy@customink.com>
2017-12-03 23:14:59 -0500

    Starter files for Wyncode's React curriculum

---
+++ src/sample-city.js
@@ -0,0 +1,98 @@
+/**
+ * sample weather data
+ */
+
+const sample_city = [
+        {
+          temp_min: 75,
+          temp_max: 83,
+          main: "Clear",
+          description: "Clear sky",
+          icon: "01d",
+          dt: "2017-11-25 18:00:00",
+          day: {
+            precipitation: "5%",
+            description: "Clear skies and pleasant",
+            icon: "01d"
+          },
+          night: {
+            precipitation: "9%",
+            description: "Partly cloudy",
+            icon: "04n"
+          }
+        },
+        {
+          temp_min: 73,
+          temp_max: 82,
+          main: "Clouds",
+          description: "Broken clouds",
+          icon: "04d",
+          dt: "2017-11-26 18:00:00",
+          day: {
+            precipitation: "47%",
+            description: "Partly sunny with a shower in spots",
+            icon: "10d"
+          },
+          night: {
+            precipitation: "25%",
+            description: "Partly cloudy",
+            icon: "04n"
+          }
+        },
+        {
+          temp_min: 72,
+          temp_max: 81,
+          main: "Rain",
+          description: "Light rain",
+          icon: "10d",
+          dt: "2017-11-27 18:00:00",
+          day: {
+            precipitation: "5%",
+            description: "Clear skies and pleasant",
+            icon: "01d"
+          },
+          night: {
+            precipitation: "9%",
+            description: "Partly cloudy",
+            icon: "04n"
+          }
+        },
+        {
+          temp_min: 75,
+          temp_max: 83,
+          main: "Clouds",
+          description: "Overcast clouds",
+          icon: "04d",
+          dt: "2017-11-28 18:00:00",
+          day: {
+            precipitation: "55%",
+            description: "Periods of clouds and sun with a brief shower or two",
+            icon: "10d"
+          },
+          night: {
+            precipitation: "40%",
+            description: "An evening shower in spots; otherwise, partly cloudy",
+            icon: "10n"
+          }
+        },
+        {
+          temp_min: 70,
+          temp_max: 79,
+          main: "Clear",
+          description: "Clear sky",
+          icon: "01d",
+          dt: "2017-11-29 18:00:00",
+          day: {
+            precipitation: "5%",
+            description: "Clear skies and pleasant",
+            icon: "01d"
+          },
+          night: {
+            precipitation: "8%",
+            description: "Partly cloudy",
+            icon: "04n"
+          }
+        }
+      ]
+
+export default sample_city;
